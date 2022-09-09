408891fcc352fa2b05259efd0e886e6099c702e4
Tolu Soneye <sammyjayden27@gmail.com>
2022-09-09 12:43:22 +0000

    slight updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -14,8 +14,9 @@ function App() {
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
+          data-testid="learning-link"
         >
-          Learn React
+          Learn Testing
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByTestId("learning-link");
   expect(linkElement).toBeInTheDocument();
+});
+
+test("url is correct", () => {
+  render(<App />);
+
+  const linkElement = screen.getByTestId("learning-link");
+
+  expect(linkElement.href).toContain("reactjs.org");
 });
